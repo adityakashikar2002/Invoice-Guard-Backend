7a806cd826f1f8ca50f090c9090cb3124fa28239
adityakashikar2002 <adityakashikar02@gmail.com>
2025-12-31 16:57:52 +0530

    invoice-guard project started

---
+++ invoiceguard/src/main/java/com/springboot/learning/invoiceguard/InvoiceGuardApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.learning.invoiceguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoiceGuardApplication.class, args);
+	}
+
+}
---
+++ invoiceguard/src/test/java/com/springboot/learning/invoiceguard/InvoiceGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.learning.invoiceguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
